842d8836afeb28ce58fb5004401bbe89c9708a83
arjunvinod004 <arjunvt004@gmail.com>
2023-04-03 10:59:30 +0530

    WEATHER APP

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function search(){
     let location = loc.value
 console.log(location);
     // fetch api
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=df556c7c5db166f2afe9cb36ed8f3d0e`)
+    fetch(`http://api.weatherapi.com/v1/current.json?key=0abfc849ac024c3f87845231230304&q=${location}&aqi=no`)
+   // https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=df556c7c5db166f2afe9cb36ed8f3d0e
     .then((result)=>{
 result.json()
 .then((data)=>{
@@ -16,16 +17,19 @@ weatherData(data)
 
 }
 function weatherData(data){
-    let temperature=data.main['temp']
-    let hum= data.main['humidity']
-    let win= data.wind['speed']
-    let pre=data.main['pressure']
-    let cit=data.name 
-    temp.innerHTML =`${temperature}&#176;`
+    let temperature= (data.current['temp_c'])
+//   data.main['temp'].splice(2)
+    
+    let hum= data.current['humidity']
+    console.log(hum);
+    let win= data.current['wind_mph']
+    let pre=data.current['pressure_mb']
+    let cit=data.location.name 
+    temp_c.innerHTML =`${temperature}&#176;`
     city.innerHTML=`${cit}`
     humidity.innerHTML= hum
-    wind.innerHTML = win
-    pressure.innerHTML=pre
+    wind_mph.innerHTML = win
+    pressure_mb.innerHTML=pre
     
   
 }
